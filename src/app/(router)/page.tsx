c25639e5f8751bb11bcca8d907abcd6d39c1df1e
m8mamad2 <m8mamad2@gmail.com>
2024-10-08 21:56:25 +0330

    init commit :)

---
+++ src/app/(router)/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      
+        <div className="flex flex-row">
+          
+          <div>
+            <h1></h1>
+          </div>
+          
+          <div></div>
+          
+        </div>
+
+    </div>
+  );
+}
+

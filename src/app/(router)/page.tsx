80c0aa9e1065b7b64d35a12677e95403b38e2aa3
m8mirali2@gmail.com <m8mamad2@gmail.com>
2024-10-10 15:42:41 +0330

    feat: add some new style

--- src/app/(router)/page.tsx
+++ src/app/(router)/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import { CiLocationArrow1 } from "react-icons/ci";
 import x from '@/app/assets/gridsvg.png'
 import bycicleImage from '@/app/assets/bycicleImageLanding.png'
+import manLandingImage from '@/app/assets/imageLanding.png'
+import playLandingIcon from '@/app/assets/playLandingIcon.png'
+import landigCircleImage from '@/app/assets/landigCircleImage.png'
+import arrowLandigLeftImage from '@/app/assets/arrowLanding1.png'
+import arrowLandigRightImage from '@/app/assets/arrowLanding2.png'
 
 export default function Home() {
   return (
@@ -40,23 +45,47 @@ export default function Home() {
             </div>
         </div>
 
-        <div className="relative flex flex-col bg-red-300 justify-center">
-          <div className="flex flex-row">
-            <div className="bg-red-200 h-40 rounded-tr-lg mr-10">LLL</div>
-            <div className="bg-red-400 h-40 rounded-tl-lg mr-2">OOo</div>
+        <div className="relative flex flex-col bg-red-300 justify-center items-center ">
+          
+          <div className="flex flex-row mt-4">
+            <div 
+              style={{ borderTopRightRadius: '60px' }}
+              className="bg-red-200  h-96 rounded-tr-lg mr-10 w-60">
+                <Image 
+                  style={{ borderTopRightRadius: '60px' }}
+                  className="w-full h-full object-cover rounded-tr-lg"
+                  src={manLandingImage}
+                  alt="landingImage"/>
+            </div>
+            <div 
+              style={{ borderTopLeftRadius: '60px' }}
+              className="flex flex-col text-center justify-start pt-10 w-60 items-center bg-gradient-to-r from-[#5DEECB] to-[#32D3AC] h-96  mr-2">
+                <Image src={playLandingIcon} alt="playLandingIcon"/>
+                <h1>FASHION AWWARD</h1>
+                <h1>WINNDER</h1>
+            </div>
           </div>
-          <div className="flex flex-row">
-            <div className="bg-purple-500 rounded-lg">LILLILIIIIII</div>
-            <div className="p-3 rounded-full">OK</div>
-            <div className="bg-green-500 rounded-lg">LILLILIIIIII</div>
+
+          <div className="flex flex-row mb-10 mt-48">
+            <div className="bg-purple-500 justify-center items-center flex py-1 rounded-lg shadow-2xl max-w-48 px-5 font-bold">PREMIMU QUALITY REQUIRE</div>
+            <div className="p-3 rounded-full">
+              <Image src={landigCircleImage} alt="landigCircleImage"/>
+            </div>
+            <div className="bg-green-500 justify-center items-center flex py-1 rounded-lg shadow-2xl max-w-48 px-5 font-bold">PREMIMU QUALITY REQUIRE</div>
           </div>
-          <div className="w-20 h-20 rounded-full absolute bg-orange-300">
-            <Image 
-              alt="la"
-              src={bycicleImage}
-              height={200}
-              width={200}/>
+          
+          <div className="w-72 h-72 flex flex-row rounded-full p-2 bg-orange-300 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/3">
+            <div className="w-full h-full rounded-full border-1 border-black p-2">
+              <div className="w-full h-full rounded-full bg-white flex justify-center items-center pt-4 pl-3">
+                <Image 
+                  alt="la"
+                  src={bycicleImage}
+                  height={230}
+                  width={230}/>
+              </div>
+            </div>
           </div>
+
         </div>
 
       </div> 
